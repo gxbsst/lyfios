da9c28e7ac415c39eebb2a84d3eb4deb2697b787
Weston <weixuhong@gmail.com>
2013-01-08 11:56:53 +0800

    init project

--- linyufang/AppDelegate.h
+++ linyufang/AppDelegate.h
@@ -1,19 +1,74 @@
-//
-//  AppDelegate.h
-//  linyufang
-//
-//  Created by weston on 1/7/13.
-//  Copyright (c) 2013 weston. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-@class ViewController;
+// NavigationBar增加方法，用于SDK5.0之前的导航栏背景颜色修改
+@interface UINavigationBar (UiNavigationBarImage)
+-(void)setBackgroundImage:(UIImage *)image;
+-(void)insertSubview:(UIView *)view atIndex:(NSInteger)index;
+@end
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@implementation UINavigationBar (UiNavigationBarImage)
+UIImageView *backgroundView;
+-(void)setBackgroundImage:(UIImage *)image
+{
+    self.tintColor = [UIColor brownColor];
+    if (image == nil) {
+        [backgroundView removeFromSuperview];
+    }
+    else
+    {
+         // 这里remove的原因是，解决tab页来回切换，navagationItem.left和right按钮被背景图片遮挡
+        if (backgroundView !=nil) {
+            [backgroundView removeFromSuperview];
+        }
+        backgroundView = [[UIImageView alloc] initWithImage: image];
+        backgroundView.tag = 10;
+        backgroundView.frame = CGRectMake(0.f, 0.f, self.frame.size.width, self.frame.size.height);
+        backgroundView.autoresizesSubviews = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        [self addSubview:backgroundView];
+    }
+}
 
-@property (strong, nonatomic) UIWindow *window;
+-(void)insertSubview:(UIView *)view atIndex:(NSInteger)index
+{
+    [super insertSubview:view atIndex:index];
+    [self sendSubviewToBack:backgroundView];
+}
 
-@property (strong, nonatomic) ViewController *viewController;
+@end
+
+@class Ivan_UITabBar;
+
+@interface AppDelegate : UIApplication<UIApplicationDelegate, UITabBarControllerDelegate>
+{
+    // 主视图
+    UIWindow *mainWindow_;
+    // 根视图控制器
+    UIViewController *mainViewController_;
+}
+
+@property(retain, nonatomic) UIWindow *mainWindow;
+@property(retain, nonatomic) UIViewController *mainViewController;
+
+#pragma mark - 全局对象
++(AppDelegate *) shareApplication;
+
+#pragma mark - 模态窗口封装
+// 调用模态窗口
+-(void)presentMoalViewController:(UIViewController *) modalViewController animated:(BOOL) animated;
+-(void)presentMoalViewController:(UIViewController *) modalViewController animated:(BOOL)animated singly:(BOOL) singly;
+
+// 取消模态窗口显示
+-(void) dismissModalViewControllerAnimated:(BOOL) animated;
+
+#pragma mark - Tabbar操作部分
+// 隐藏Tabbar
+-(void) hiddenTabBar:(BOOL) hidden;
+
+// 获取TabbarController
+
+-(Ivan_UITabBar *) tabBarController;
+
+// 登录成功的响应函数
+-(void) loginSuccessful:(NSNotification *) notifaction;
 
 @end
--- linyufang/AppDelegate.m
+++ linyufang/AppDelegate.m
@@ -1,52 +1,161 @@
-//
-//  AppDelegate.m
-//  linyufang
-//
-//  Created by weston on 1/7/13.
-//  Copyright (c) 2013 weston. All rights reserved.
-//
-
 #import "AppDelegate.h"
-
-#import "ViewController.h"
+#import "Ivan_UITabBar.h"
+#import "HomeController.h"
+#import "CategoryController.h"
+#import "CollectionController.h"
+#import "SearchController.h"
+#import "LoginController.h"
 
 @implementation AppDelegate
+@synthesize mainViewController = mainViewController_;
+@synthesize mainWindow = mainWindow_;
 
++(AppDelegate *) shareApplication
+{
+    return (AppDelegate *) [UIApplication sharedApplication];
+}
+
+- (void) presentModalViewController:(UIViewController *) modalViewController
+{
+    [self presentMoalViewController:modalViewController animated:YES singly:YES];
+}
+
+-(void) presentMoalViewController:(UIViewController *)modalViewController animated:(BOOL)animated
+{
+    [self presentMoalViewController:modalViewController animated:YES singly:YES];
+}
+
+- (void) _presentModalViewControllerWithInfo:(NSDictionary *) info
+{
+    UIViewController *modalViewController = [info objectForKey:@"modalViewController"];
+    BOOL animated = [[info objectForKey:@"animated"] boolValue];
+    
+    [self presentMoalViewController:modalViewController animated:animated singly:YES];
+}
+
+-(void) presentMoalViewController:(UIViewController *)modalViewController animated:(BOOL)animated singly:(BOOL)singly
+{
+   	if (singly && self.modalViewController) {
+		[self dismissModalViewControllerAnimated:animated];
+		if (animated) {
+			NSDictionary *info = [[NSDictionary alloc] initWithObjectsAndKeys:modalViewController, @"modalViewController", [NSNumber numberWithBool:animated], @"animated", nil];
+			[self performSelector:@selector(_presentModalViewControllerWithInfo:) withObject:info afterDelay:0.5];
+			return;
+		}
+	}
+    
+//	[mainViewController_ presentModalViewController:modalViewController animated:animated];
+    [mainViewController_ presentViewController:modalViewController animated:YES completion:NULL];
+}
+
+// 取消模态窗口显示
+- (void)dismissModalViewControllerAnimated:(BOOL) animated
+{
+    [mainViewController_ dismissViewControllerAnimated:animated completion:NULL];
+}
+
+// 获取模态窗口
+- (UIViewController *) modalViewController
+{
+//    return mainViewController_.modalViewController;
+    return mainViewController_.presentedViewController;
+
+}
+
+#pragma mark - 操作Tabbar
+// 获取TabbarController指针
+-(Ivan_UITabBar *) tabBarController
+{
+    if ([mainViewController_ isKindOfClass:([Ivan_UITabBar class])]) {
+        return (Ivan_UITabBar *) mainViewController_;
+    } else {
+        return nil;
+    }
+}
+
+// 隐藏Tabbar
+- (void) hiddenTabBar:(BOOL)hidden
+{
+    Ivan_UITabBar *tabBar = [self tabBarController];
+    
+    if (nil != tabBar) {
+        [tabBar hiddenTabBar:hidden];
+    }
+}
+
+
+// 收到登录成功的通知，在发送登录请求的时候，可以在SHHLoginView中添加一个等待视图。
+// 这里只是一个简单的demo，点击登录直接发送登录成功通知。
+- (void) loginSuccessful:(NSNotification *)notification
+{
+    [UIView beginAnimations:@"View Flip" context:nil];
+    [UIView setAnimationDuration:0.6f];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+    
+    UIViewAnimationTransition transition;
+    transition = UIViewAnimationTransitionCurlUp;
+    
+    [UIView setAnimationTransition:transition forView:[[LoginController defaultController].loginViewController view].superview cache:YES];
+    
+    [[[LoginController defaultController].loginViewController view] removeFromSuperview];
+    // 增加Tabbar,如果不需要登录界面，把下面这条代码放到didFinishLaunchingWithOptions中
+    [mainWindow_ addSubview:mainViewController_.view];
+    [UIView commitAnimations];
+}
+
+#pragma mark - 应用程序委托
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
-    self.window.rootViewController = self.viewController;
-    [self.window makeKeyAndVisible];
+    // 增加消息通知
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(loginSuccessful:)
+                                                 name:@"LoginSuccessful"
+                                               object:nil];
+    
+    // 显示主界面
+    mainWindow_ = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    // 底部Tabar
+    Ivan_UITabBar *tabBarController = [[Ivan_UITabBar alloc] initWithNibName:nil bundle:nil];
+    tabBarController.delegate = self;
+    
+    // 设置Tabar选中的效果图片
+    NSMutableDictionary *selectImageDict = [[NSMutableDictionary alloc] initWithCapacity:5];
+    
+    NSString *path = [NSString stringWithFormat:@"recomment_h"];
+    NSString *key = [NSString stringWithFormat:@"0"];
+    [selectImageDict setObject:path forKey:key];
+    NSString *path1 = [NSString stringWithFormat:@"partition_h"];
+    NSString *key1 = [NSString stringWithFormat:@"1"];
+    [selectImageDict setObject:path1 forKey:key1];
+    NSString *path2 = [NSString stringWithFormat:@"space_h"];
+    NSString *key2 = [NSString stringWithFormat:@"2"];
+    [selectImageDict setObject:path2 forKey:key2];
+    NSString *path3 = [NSString stringWithFormat:@"collect_h"];
+    NSString *key3 = [NSString stringWithFormat:@"3"];
+    [selectImageDict setObject:path3 forKey:key3];
+    NSString *path4 = [NSString stringWithFormat:@"search_h"];
+    NSString *key4 = [NSString stringWithFormat:@"4"];
+    [selectImageDict setObject:path4 forKey:key4];
+    
+    tabBarController.dictBtnImg = selectImageDict;
+//    [selectImageDict release];
+    
+    // 设置tabBar对应的viewControllers,此处设置的导航
+    NSArray *viewControllers = [[NSArray alloc] initWithObjects:[HomeController defaultController].homeNavigationController, [CategoryController defaultController].categoryNavigationController, [SearchController defaultController].searchNavigationController,[CollectionController defaultController].collectNavigationController, [SearchController defaultController].searchNavigationController, nil];
+    
+    tabBarController.viewControllers = viewControllers;
+//    [viewControllers release];
+    
+    tabBarController.selectedIndex = 0;
+    mainViewController_ = tabBarController;
+    
+    [mainWindow_ addSubview:[[LoginController defaultController].loginViewController view]];
+    
+    [mainWindow_ makeKeyAndVisible];
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
 
 @end
