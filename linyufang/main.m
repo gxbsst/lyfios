e996c0a434295116b5c898b2528f79421d1fb65d
Weston <weixuhong@gmail.com>
2013-01-08 11:57:29 +0800

    aaa

--- linyufang/main.m
+++ linyufang/main.m
@@ -1,18 +1,28 @@
+////
+////  main.m
+////  linyufang
+////
+////  Created by weston on 1/7/13.
+////  Copyright (c) 2013 weston. All rights reserved.
+////
 //
-//  main.m
-//  linyufang
+//#import <UIKit/UIKit.h>
 //
-//  Created by weston on 1/7/13.
-//  Copyright (c) 2013 weston. All rights reserved.
+//#import "AppDelegate.h"
 //
+//int main(int argc, char *argv[])
+//{
+//    @autoreleasepool {
+//        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+//    }
+//}
 
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
-
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv, @"AppDelegate", @"AppDelegate");
     }
 }
